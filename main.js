7d581d3bd7a06801de3c9673c50b09f3d1f08d5d
Luizzzin <luizhenribarbodias@gmail.com>
2023-09-18 11:48:23 -0300

    up
    up

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function start(){
-    let start = document.getElementById("start")
-    let resultado = document.querySelector(".resultado");
-    
+function start() {
+    let ramdola = Math.floor(Math.random()* (100 - 1) - 1);
 
-    
+
+}
+
+
+function verificar() {
+    let palpite = Number(prompt("DE O PALPITE DE SEU NUMERO"))
+
+
+
 }
