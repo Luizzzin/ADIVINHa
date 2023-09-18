698014ea39a71c6103cecad9eb3d43fbc069af6a
Luizzzin <luizhenribarbodias@gmail.com>
2023-09-18 09:56:23 -0300

    up
    up

---
+++ main.js
@@ -0,0 +1,7 @@
+function start(){
+    let start = document.getElementById("start")
+    let resultado = document.querySelector(".resultado");
+    let palpite = document.getElementById("palpite")
+
+    if ()
+}
